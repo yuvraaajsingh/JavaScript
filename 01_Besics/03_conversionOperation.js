3a747b7b0b384e8bc1c0257347d771169d5a9ab2
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-02-19 15:35:58 +0000

    config done

--- 01_Besics/03_conversionOperation.js
+++ 01_Besics/03_conversionOperation.js
@@ -1,10 +1,24 @@
 let score = "33";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+// ******************Operation***************
+
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue)
+
+// console.log("2"+2+2);  //222
+// console.log(2+7+"8");   //98
