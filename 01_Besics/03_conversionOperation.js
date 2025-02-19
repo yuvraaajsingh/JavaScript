baa20d63ab07a573fc03d5d19911e47ea3635ceb
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-02-19 15:23:24 +0000

    config done

---
+++ 01_Besics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
