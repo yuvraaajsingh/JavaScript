1bfac2202ca8a10d86bea4572a697d11e5936c5b
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-02-19 15:09:57 +0000

    config done

--- 01_Besics/02_datatypes.js
+++ 01_Besics/02_datatypes.js
@@ -1,4 +1,20 @@
 "use strict";  //treat  all js code as newer version.
 
 // alert(3+3)  // We are using node.js, not browser.
+let name = "yuvraj";
+let age=24;
 
+//  **Premitative DataTypes**
+// number
+// bigint
+// string
+// boolean
+// null  ==> standalone value
+// undefined
+// symbol  =>  unique
+
+
+//  object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
